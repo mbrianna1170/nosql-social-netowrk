46eb4a89c3129935d373b8e1868f638fe734190d
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-01 16:21:02 -0500

    created user model and controller obj

---
+++ controllers/user-controller.js
@@ -0,0 +1,50 @@
+const { response } = require("express");
+const { User } = require("../models");
+
+const userController = {
+  // get all users
+  getAllUser(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // get one user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then((dbUserData) => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // create a user
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+  // update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = userController;
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+// creates Schema for User model
+const UserSchema = new Schema({
+    username: {
+        type: String, 
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email : {
+        type: String,
+        unique: true,
+        required: true
+    },
+    thoughts: {
+
+    },
+    friends: {
+
+    }
+});
+
+// create the User model using the UserSchema 
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
